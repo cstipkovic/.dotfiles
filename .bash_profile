151aaafe2b1451643b494e2363add9f9019313dc
Clauber Stipkovic <kovic@K-Book.local>
2017-09-28 15:23:46 -0300

    Updating the script to install dotfiles

--- .bash_profile
+++ .bash_profile
@@ -9,9 +9,9 @@ c_green=`tput setaf 2`
 c_sgr0=`tput sgr0`
 
 branch_color () {
-    if git rev-parse --git-dir >/dev/null 2>&1 then
-        color=""
-        if git diff --quiet 2>/dev/null >&2 then
+    if [ git rev-parse --git-dir >/dev/null 2>&1 ]; then
+        color=``
+        if [ git diff --quiet 2>/dev/null >&2 ]; then
            color=${c_red}
         else
           color=${c_green}
@@ -23,7 +23,7 @@ branch_color () {
 }
 
 parse_git_branch () {
-    if git rev-parse --git-dir >/dev/null 2>&1 then
+    if [ git rev-parse --git-dir >/dev/null 2>&1 ]; then
       gitver=" ("$(git branch 2>/dev/null| sed -n '/^\*/s/^\* //p')")"
     else
         return 0
@@ -32,6 +32,6 @@ parse_git_branch () {
 }
 
 #It's important to escape colors with \[ to indicate the length is 0
-PS1='${c_green}[\u@\[ \]\W\[\]\[\[$(branch_color)\] $(parse_git_branch)\[\]]${c_sgr0}\n '
+PS1='${c_green}[\u@\[ \]\W\[\]\[\[$(branch_color)\] $(parse_git_branch)\[\]]${c_sgr0}\n'
 
 source ~/.git-completion.bash
--- dotfiles_install.sh
+++ dotfiles_install.sh
@@ -1,43 +1,23 @@
 #!/bin/bash
 # The dotfile initial
 
+echo "Creating .bash_profile"
+ln -s ~/.dotfiles/.bash_profile ~/
+
 # Mac OS X Darwin
-_darwin_os() {
-  if [ ! -f ~/.bash_profile ] then
-    curl -Lo ~/.git-completion.bash https://github.com/git/git/raw/master/contrib/completion/git-completion.bash
-    source .git-completion.bash
-    echo "Creating symbolyc link for .bash_profile"
-    ln -s ~/.bash/.bash_profile ~/.bash_profile
-    echo "source ~/.git-completion.bash" >> ~/.bash_profile
-  else
-    echo "Exists .bash_profile"
-  fi
-
-  #_dotfile
-}
-
-# GNU Linux
-_linux_os() {
-  echo "Linux"
-  _dotfile
-}
+#_darwin_os() {
+#}
 
 # Common functions
-_dotfile() {
-  if [ ! -f ~/.bashrc ] then
-    echo "Creating symbolyc link for .bashrc"
-    ln -s ~/.bash/.bashrc ~/.bashrc
-  else
-    echo "Exists .bashrc"
-  fi
-}
+#_dotfile() {
+#}
 
 # Get the OS Name
 os_name=$(uname -s)
 
-if [ $os_name == "Darwin" ] then
-  _darwin_os
-else
-  _linux_os
-fi
+#if [ $os_name == "Darwin" ] then
+#  _darwin_os
+#else
+#  _linux_os
+#fi
 
