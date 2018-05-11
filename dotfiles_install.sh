b1a2017f428c69c0d3148d6fe10909e17f8f0e10
Clauber Stipkovic <clauber.halic@gmail.com>
2018-05-11 03:09:53 -0300

    Fixing vim-plug and linux commands

--- dotfiles_install.sh
+++ dotfiles_install.sh
@@ -1,23 +1,33 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # The dotfile initial
 
-echo "Creating .bash_profile"
-ln -s ~/.dotfiles/.bash_profile ~/
-
-# Mac OS X Darwin
-#_darwin_os() {
-#}
-
-# Common functions
-#_dotfile() {
-#}
-
-# Get the OS Name
 os_name=$(uname -s)
 
-#if [ $os_name == "Darwin" ] then
-#  _darwin_os
-#else
-#  _linux_os
-#fi
+# Linux
+_linux_os() {
+	ln -s ~/.dotfiles/.bashrc ~/
+	ln -s ~/.dotfiles/vim/.vimrc ~/
+}
+
+if [ $os_name = "Darwin" ] 
+then
+  _darwin_os
+else
+  _linux_os
+fi
+
+echo "Getting Vim-Plug"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Mac OS X Darwin
+_darwin_os() {
+	ln -s ~/.dotfiles/.bash_profile ~/
+}
+
+# Common functions
+_dotfile() {
+	echo "dotfiles"
+}
+
 
